1c386c38e890986ce40d2171ecdd03692c6a444c
Muhammad Faiz <muhammad.faiz@infrontconsulting.com.my>
2026-09-18 04:26:54 +0800

    Show blue TP badge for PHD-confirmed Tiada Pengeluaran forms
    Row 7 of the PHD icon spec: once PHD confirms and the package is
    sent, a form that was originally submitted as Tiada Pengeluaran
    should keep showing the tpbiru.png badge instead of the generic
    circle_check.png, matching the existing pattern already used for
    the "Lulus" status.

--- resources/views/admins/PHD/senarai-tugasan-5D.blade.php
+++ resources/views/admins/PHD/senarai-tugasan-5D.blade.php
@@ -183,11 +183,19 @@
                                                             title="Borang tidak lengkap"></i></a>
                                                     @elseif($data->status == 'Dihantar ke IPJPSM')
                                                         @if ($packageSent)
-                                                            <a href="{{ route('phd.shuttle-5-view-formD-phd', $data->id) }}">
-                                                                <img src="{{ asset('circle_check.png') }}" height='30px'
-                                                                    data-toggle="tooltip" data-placement="bottom"
-                                                                    title="Borang telah disahkan PHD">
-                                                            </a>
+                                                            @if ($data->tiada_pengeluaran == 1)
+                                                                <a href="{{ route('phd.shuttle-5-view-formD-phd', $data->id) }}">
+                                                                    <img src="{{ asset('tpbiru.png') }}" height='30px'
+                                                                        data-toggle="tooltip" data-placement="bottom"
+                                                                        title="Borang telah disahkan PHD - Tiada Pengeluaran">
+                                                                </a>
+                                                            @else
+                                                                <a href="{{ route('phd.shuttle-5-view-formD-phd', $data->id) }}">
+                                                                    <img src="{{ asset('circle_check.png') }}" height='30px'
+                                                                        data-toggle="tooltip" data-placement="bottom"
+                                                                        title="Borang telah disahkan PHD">
+                                                                </a>
+                                                            @endif
                                                         @else
                                                             <img src="{{ asset('package.png') }}" height='40px'
                                                                 data-toggle="tooltip" data-placement="bottom"
